087c9cb3e9942b48d065018b5b03ea99e4386f48
steveolsen55 <steve.olsen@email.edcc.edu>
2017-04-16 20:30:22 -0700

    Add files via upload

---
+++ BigRobotJoystick_Revised_for_Xbee_Receiving/BigRobotJoystick_Revised_for_Xbee_Receiving.ino
@@ -0,0 +1,98 @@
+#include <Servo.h> 
+
+Servo leftMotor;
+Servo rightMotor;
+
+
+const int RED_LED = 11;
+const int GREEN_LED = 12;
+
+const int BUTTON = A5;             
+
+
+const int MOTOR_PIN_LEFT = 8;
+const int MOTOR_PIN_RIGHT = 11;
+
+
+const int MOTOR_VALUE_MIN = 50;
+const int MOTOR_VALUE_MAX = 130;
+const int MOTOR_VALUE_STOP = 90;
+
+const int NUMBER_OF_BYTES_IN_A_COMMAND = 4;
+const int SERIAL_COMMAND_SET_LEFT_MOTOR = 254;
+const int SERIAL_COMMAND_SET_RIGHT_MOTOR = 255;
+
+const long SERIAL_DATA_SPEED_38400_BPS = 38400;
+
+int leftMotorVal;
+int rightMotorVal;
+
+ boolean turnOnOff = 0;
+ int valButton;
+ 
+ void setup()
+{
+    Serial.begin(SERIAL_DATA_SPEED_38400_BPS);
+    
+    
+    pinMode(RED_LED,OUTPUT);            
+    pinMode(GREEN_LED,OUTPUT);
+    
+    
+    leftMotor.attach (MOTOR_PIN_LEFT);
+    leftMotor.write(MOTOR_VALUE_STOP);
+    
+   
+    rightMotor.attach(MOTOR_PIN_RIGHT);
+    rightMotor.write(MOTOR_VALUE_STOP);
+    
+    
+}
+
+void loop()
+{
+  static char rightMotor = 0;
+  static char leftMotor = 0;
+  //Serial.println( Serial.available());
+  if (Serial.available() > NUMBER_OF_BYTES_IN_A_COMMAND)
+  {
+    int incomingByte = Serial.read(); 
+    Serial.println( incomingByte);
+    if(SERIAL_COMMAND_SET_LEFT_MOTOR == incomingByte)
+    {
+      leftMotor = Serial.read();
+    }
+    if(SERIAL_COMMAND_SET_RIGHT_MOTOR == incomingByte)
+    {
+      rightMotor = Serial.read();
+    }
+   }
+   motor_setValues(leftMotor,rightMotor);
+}
+//************************ Subroutines ****************************
+
+
+void motor_setValues (int normalizedLeft, int normalizedRight)
+{
+  if (normalizedLeft == 0)
+  {
+    leftMotorVal = MOTOR_VALUE_STOP;
+  }
+  else
+  {
+     leftMotorVal = map(normalizedLeft,-100,100,MOTOR_VALUE_MIN,MOTOR_VALUE_MAX);
+  }
+  
+   if (normalizedRight == 0)
+  {
+     rightMotorVal = MOTOR_VALUE_STOP;
+  }
+  else
+  {
+     rightMotorVal = map(normalizedRight,-100,100,MOTOR_VALUE_MIN,MOTOR_VALUE_MAX);
+  }
+  
+  leftMotor.write(leftMotorVal);
+  rightMotor.write(rightMotorVal);
+}
+  
---
+++ BigRobotJoystick_Revised_for_Xbee_Sending/BigRobotJoystick_Revised_for_Xbee_Sending.ino
@@ -0,0 +1,105 @@
+
+ 
+const int MOTOR_VALUE_STOP = 0;
+
+const int X_JOYSTICK_MIN = 0;
+const int X_JOYSTICK_MAX = 1023;
+
+const int Y_JOYSTICK_MIN = 50;
+const int Y_JOYSTICK_MAX = 1023;
+
+const int SERIAL_COMMAND_SET_LEFT_MOTOR = 254;
+const int SERIAL_COMMAND_SET_RIGHT_MOTOR = 255;
+
+const int DEAD_ZONE = 15;
+
+const unsigned long TIME_BETWEEN_GET_DATA = 100;
+const long SERIAL_DATA_SPEED_38400_BPS = 38400;
+     
+ int JOYSTICK_X = A1;     
+ int JOYSTICK_Y = A0; 
+ 
+ 
+ int UDvalue;      
+ int LRvalue;      
+ int LeftServoVal;                 
+ int RightServoVal;   
+ 
+ 
+ unsigned long previousTime;
+
+void setup()
+{
+    Serial.begin(SERIAL_DATA_SPEED_38400_BPS);
+         
+    previousTime = millis();
+}
+
+void loop()
+{   
+      int UDvalue = 0;
+      int LRvalue = 0;
+      
+      if (millis()- previousTime >= TIME_BETWEEN_GET_DATA)
+      {
+        UDvalue = analogRead(JOYSTICK_Y);           
+        LRvalue = analogRead(JOYSTICK_X);   
+    
+        UDvalue = map(UDvalue,Y_JOYSTICK_MIN,Y_JOYSTICK_MAX,-100,100);  
+        LRvalue = map(LRvalue,X_JOYSTICK_MIN,X_JOYSTICK_MAX,-100,100);    
+        
+       if(UDvalue >= -DEAD_ZONE && UDvalue <= DEAD_ZONE && LRvalue <=  DEAD_ZONE && LRvalue >= -DEAD_ZONE) 
+       {           
+        
+          SendNewMotorValues(MOTOR_VALUE_STOP,MOTOR_VALUE_STOP);
+          previousTime = millis();
+        }
+        else                                   
+        {                                         
+          moveRobot(LRvalue,UDvalue);
+          previousTime = millis();
+        }
+      }
+}
+   
+
+
+//****************************** SUBROUTINES ************************************
+  void SendNewMotorValues(char left, char right)
+      {
+        Serial.write (SERIAL_COMMAND_SET_LEFT_MOTOR);
+        Serial.write (left);
+                  
+        Serial.write (SERIAL_COMMAND_SET_RIGHT_MOTOR);
+        Serial.write (right);
+        Serial.print(left,DEC);
+        Serial.print("   ");
+        Serial.println(right,DEC);
+      }
+      
+//*************** Calculate motor values **************************************************      
+  void moveRobot (int valueX, int valueY)//subroutine to send pulses to the correct servo.
+  {
+    //Use the X direction to set the forward and backward motion
+    int leftMotorVal = valueY;
+    int rightMotorVal = valueY;
+    
+    //Apply the Y as a "twist" effect. (about 40% of the calculated value)
+    if (valueY >=0)
+    {
+      leftMotorVal = leftMotorVal + (valueX * 4)/10;
+      rightMotorVal = rightMotorVal - (valueX * 4)/10;
+    }
+    else
+    {
+      leftMotorVal = leftMotorVal - (valueX * 4)/10;
+      rightMotorVal = rightMotorVal + (valueX * 4)/10;
+    }
+      leftMotorVal = constrain(leftMotorVal,-100,100);
+      rightMotorVal =constrain(rightMotorVal,-100,100);
+     
+      SendNewMotorValues(leftMotorVal,rightMotorVal);
+  }
+
+
+
