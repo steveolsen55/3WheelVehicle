0a7db6be14b3e6cb77eb5870ca1c18f13701770d
a.gilliland9771 <a.gilliland9771@edmail.edcc.edu>
2017-05-08 13:00:07 -0700

    added stop for non comm

--- BigRobotJoystick_Revised_for_Xbee_Receiving/BigRobotJoystick_Revised_for_Xbee_Receiving.ino
+++ BigRobotJoystick_Revised_for_Xbee_Receiving/BigRobotJoystick_Revised_for_Xbee_Receiving.ino
@@ -1,34 +1,34 @@
 /*
- *   James Beaver, Anthony G., Stephen A.
- *   May 2, 2017
- *   ETEC 290 Capstone project
- *   
- *  Robot control program.  
- *    Hand controller joystick position communication via XBee 802.15 radio protocol.    
- *    A 8 bit state variable is used to track status of LEDs, button presses, and other controls.
- *    If the Up/Down joystick is pushed forward --> throttle control
- *                         ... pulled back    --> throttle to zero and apply brake
- *    Left/Right joystick input is currently being limited to 40% of full range in the Hand Controller program.  
- *    
- *  Hardware setup:
- *    Arduino Pro Mini 5v 16MHz microcontroller, 
- *    XBee series 1 1mw U.FL Connection, on a SparkFun 'XBee Explorer Regulated' board
- *    two continuous rotation servos for testing.    
- *    LEDs for status.
- *    LiPo 22.6v battery, regulated to 5v for electronics.
- *    
- *    Robot will use an 80mm Electric Ducted Fan (EDF) with an Electronic Speed Control (ESC) for propulsion.
- *    Braking will be provided by servo controlled mechanical brake on the rear wheel.
- *    Steering is provided by servo controlled rack and pinnion steering mechanism on the front wheels.
- *   
- *    TBD - Turbo boost control, to provide full speed boost on straight segments
- *        - EDF / ECS throttle limit, proportionally to turn rate, max un-boosted limit TBD
- *        - Loss of communication must set throttle to zero and apply brakes.
- *   
- *   
- */
+     James Beaver, Anthony G., Stephen A.
+     May 2, 2017
+     ETEC 290 Capstone project
 
-#include <Servo.h> 
+    Robot control program.
+      Hand controller joystick position communication via XBee 802.15 radio protocol.
+      A 8 bit state variable is used to track status of LEDs, button presses, and other controls.
+      If the Up/Down joystick is pushed forward --> throttle control
+                           ... pulled back    --> throttle to zero and apply brake
+      Left/Right joystick input is currently being limited to 40% of full range in the Hand Controller program.
+
+    Hardware setup:
+      Arduino Pro Mini 5v 16MHz microcontroller,
+      XBee series 1 1mw U.FL Connection, on a SparkFun 'XBee Explorer Regulated' board
+      two continuous rotation servos for testing.
+      LEDs for status.
+      LiPo 22.6v battery, regulated to 5v for electronics.
+
+      Robot will use an 80mm Electric Ducted Fan (EDF) with an Electronic Speed Control (ESC) for propulsion.
+      Braking will be provided by servo controlled mechanical brake on the rear wheel.
+      Steering is provided by servo controlled rack and pinnion steering mechanism on the front wheels.
+
+      TBD - Turbo boost control, to provide full speed boost on straight segments
+          - EDF / ECS throttle limit, proportionally to turn rate, max un-boosted limit TBD
+          - Loss of communication must set throttle to zero and apply brakes.
+
+
+*/
+
+#include <Servo.h>
 
 Servo throttleMotor;
 Servo steeringMotor;
@@ -54,45 +54,45 @@ const int MOTOR_VALUE_THROTTLE_TURBO = 100;  // maximum turbo boosted throttle l
 
 const int NUMBER_OF_BYTES_IN_A_COMMAND = 8;
 const int SERIAL_COMMAND_SET_CMD = 252;          // serial data code - next byte is a command byte
-const int SERIAL_COMMAND_SET_THROTTLE = 253;     // serial data code - next byte is throttle setting 
-const int SERIAL_COMMAND_SET_STEERING_POS = 254; // serial data code - next byte is steering position 
+const int SERIAL_COMMAND_SET_THROTTLE = 253;     // serial data code - next byte is throttle setting
+const int SERIAL_COMMAND_SET_STEERING_POS = 254; // serial data code - next byte is steering position
 const int SERIAL_COMMAND_SET_BRAKE_POS = 255;    // serial data code - next byte is brake setting
 
 const long SERIAL_DATA_SPEED_BPS = 57600;   // Baud rate = 57600 for Capstone Xbee's
 
 byte debug = 1;      //  set to 1 to send debug output to Serial Monitor
 
-int throttleMotorVal;       // 
+int throttleMotorVal;       //
 int steeringMotorVal;
 int brakeMotorVal;
 
-byte state_machine = 0x00;  //  Bit 0 = Robot enabled, set for ON, clear for OFF  
-                            //  Bit 1 = turbo boost, set for ON, clear for OFF
-                            //  Set each bit to 1 when on.  This var tracks the state machine status.
+byte state_machine = 0x00;  //  Bit 0 = Robot enabled, set for ON, clear for OFF
+//  Bit 1 = turbo boost, set for ON, clear for OFF
+//  Set each bit to 1 when on.  This var tracks the state machine status.
 
 void setup()
 {
-    pinMode(RED_LED,OUTPUT);            
-    pinMode(GREEN_LED,OUTPUT);
-    pinMode(BLUE_LED,OUTPUT);
-    digitalWrite(GREEN_LED, LOW);
-    digitalWrite(RED_LED, HIGH); 
-    digitalWrite(BLUE_LED, LOW); 
-   
-    throttleMotor.attach(MOTOR_PIN_THROTTLE);  
-    steeringMotor.attach (MOTOR_PIN_STEERING);
-    brakeMotor.attach(MOTOR_PIN_BRAKE);
-    stopRobot();
+  pinMode(RED_LED, OUTPUT);
+  pinMode(GREEN_LED, OUTPUT);
+  pinMode(BLUE_LED, OUTPUT);
+  digitalWrite(GREEN_LED, LOW);
+  digitalWrite(RED_LED, HIGH);
+  digitalWrite(BLUE_LED, LOW);
 
-//  -->>  initialize the Electronic Speed Controller (ESC) here <<--
+  throttleMotor.attach(MOTOR_PIN_THROTTLE);
+  steeringMotor.attach (MOTOR_PIN_STEERING);
+  brakeMotor.attach(MOTOR_PIN_BRAKE);
+  stopRobot();
 
-    throttleMotor.write(0);
-    delay (20);
-    throttleMotor.write(180);
-    delay (10);
-    throttleMotor.write(0);
-  
-    Serial.begin(SERIAL_DATA_SPEED_BPS);
+  //  -->>  initialize the Electronic Speed Controller (ESC) here <<--
+
+  throttleMotor.write(0);
+  delay (20);
+  throttleMotor.write(180);
+  delay (10);
+  throttleMotor.write(0);
+
+  Serial.begin(SERIAL_DATA_SPEED_BPS);
 }
 
 void loop()
@@ -101,65 +101,70 @@ void loop()
   static char steeringMotor = 0;
   static char brakeMotor = 0;
 
-//  if(debug == 1)
-//      Serial.println( Serial.available());
+  //  if(debug == 1)
+  //      Serial.println( Serial.available());
 
   if (Serial.available() >= NUMBER_OF_BYTES_IN_A_COMMAND)
   {
-    int incomingByte = Serial.read(); 
-    
-//  if(debug == 1)
-//    Serial.println( incomingByte);
+    int incomingByte = Serial.read();
 
-   switch (incomingByte)
-   {
-     case SERIAL_COMMAND_SET_CMD:
-     {
-        state_machine = Serial.read();
-        break;
-     }
-     case  SERIAL_COMMAND_SET_THROTTLE:
-     {
-        throttleMotor = Serial.read();
-        break;
-     }
-     case SERIAL_COMMAND_SET_STEERING_POS:
-     {
-        steeringMotor = Serial.read();
-        break;
-     }
-     case SERIAL_COMMAND_SET_BRAKE_POS:
-     {
-        brakeMotor = Serial.read();
-        break;
-     }
-   }
+    //  if(debug == 1)
+    //    Serial.println( incomingByte);
 
-if (debug == 1)
-{
-   Serial.print("state_machine ="); Serial.print(state_machine, BIN);
-   Serial.print("\t");
-   Serial.print("throttle = "); Serial.print(throttleMotor);
-   Serial.print("\t");
-   Serial.print("steering = "); Serial.print(steeringMotor);
-   Serial.print("\t");
-   Serial.print("brake = "); Serial.println(brakeMotor);
+    switch (incomingByte)
+    {
+      case SERIAL_COMMAND_SET_CMD:
+        {
+          state_machine = Serial.read();
+          break;
+        }
+      case  SERIAL_COMMAND_SET_THROTTLE:
+        {
+          throttleMotor = Serial.read();
+          break;
+        }
+      case SERIAL_COMMAND_SET_STEERING_POS:
+        {
+          steeringMotor = Serial.read();
+          break;
+        }
+      case SERIAL_COMMAND_SET_BRAKE_POS:
+        {
+          brakeMotor = Serial.read();
+          break;
+        }
+    }
+    if (Serial.available() == 0)
+    {
+      throttleMotor = 0;
+      brakeMotor = 45;
+    }
+  
+  if (debug == 1)
+  {
+    Serial.print("state_machine ="); Serial.print(state_machine, BIN);
+    Serial.print("\t");
+    Serial.print("throttle = "); Serial.print(throttleMotor);
+    Serial.print("\t");
+    Serial.print("steering = "); Serial.print(steeringMotor);
+    Serial.print("\t");
+    Serial.print("brake = "); Serial.println(brakeMotor);
+  }
+
 }
-
-  }
-  if( bitRead(state_machine,0) == true )   // Green status: pulse servos
-  {
-    digitalWrite(GREEN_LED, HIGH);
-    digitalWrite(RED_LED, LOW);  
-    motor_setValues(throttleMotor,steeringMotor,brakeMotor);   
-  }
-  if( bitRead(state_machine,0) == false )    //  All Stop!!
-  {
-     digitalWrite(GREEN_LED, LOW);
-     digitalWrite(RED_LED, HIGH);
-     digitalWrite(BLUE_LED,LOW);
-     stopRobot();
-  }
+if ( bitRead(state_machine, 0) == true ) // Green status: pulse servos
+{
+  digitalWrite(GREEN_LED, HIGH);
+  digitalWrite(RED_LED, LOW);
+  motor_setValues(throttleMotor, steeringMotor, brakeMotor);
+}
+if ( bitRead(state_machine, 0) == false )  //  All Stop!!
+{
+  digitalWrite(GREEN_LED, LOW);
+  digitalWrite(RED_LED, HIGH);
+  digitalWrite(BLUE_LED, LOW);
+  stopRobot();
+}
 }
 //************************ Subroutines ****************************
 
@@ -171,43 +176,43 @@ void motor_setValues (int throttle, int steering, int brake)
   }
   else
   {
-     throttleMotorVal = map(throttle,100,0, MOTOR_VALUE_THROTTLE_MAX, MOTOR_VALUE_THROTTLE_MIN );
-     
-     if ( bitRead(state_machine,1) == true )   // turbo boost on
-     {
-        throttleMotorVal = MOTOR_VALUE_THROTTLE_TURBO; 
-        digitalWrite(BLUE_LED, HIGH);
-     }
-     else
-     {
-        digitalWrite(BLUE_LED,LOW);
-     }
+    throttleMotorVal = map(throttle, 100, 0, MOTOR_VALUE_THROTTLE_MAX, MOTOR_VALUE_THROTTLE_MIN );
+
+    if ( bitRead(state_machine, 1) == true )  // turbo boost on
+    {
+      throttleMotorVal = MOTOR_VALUE_THROTTLE_TURBO;
+      digitalWrite(BLUE_LED, HIGH);
+    }
+    else
+    {
+      digitalWrite(BLUE_LED, LOW);
+    }
   }
-  
+
   if (steering == 0)
   {
-     steeringMotorVal = MOTOR_VALUE_CENTER;
+    steeringMotorVal = MOTOR_VALUE_CENTER;
   }
   else
   {
-     steeringMotorVal = map(steering,-100,100,MOTOR_VALUE_MIN,MOTOR_VALUE_MAX);
+    steeringMotorVal = map(steering, -100, 100, MOTOR_VALUE_MIN, MOTOR_VALUE_MAX);
   }
   if (brake == 0)
   {
-     brakeMotorVal = MOTOR_VALUE_CENTER;
+    brakeMotorVal = MOTOR_VALUE_CENTER;
   }
   else
   {
-     brakeMotorVal = map(brake,-100,0,MOTOR_VALUE_NO_BRAKE,MOTOR_VALUE_FULL_BRAKE);
+    brakeMotorVal = map(brake, -100, 0, MOTOR_VALUE_NO_BRAKE, MOTOR_VALUE_FULL_BRAKE);
   }
 
-  if(debug == 1)
-  {  /*
-     Serial.print("throttle = "); Serial.print(throttleMotorVal);
-     Serial.print("\t");
-     Serial.print("steering = "); Serial.print(steeringMotorVal);
-     Serial.print("\t");
-     Serial.print("brake = "); Serial.println(brakeMotorVal);   */
+  if (debug == 1)
+  { /*
+      Serial.print("throttle = "); Serial.print(throttleMotorVal);
+      Serial.print("\t");
+      Serial.print("steering = "); Serial.print(steeringMotorVal);
+      Serial.print("\t");
+      Serial.print("brake = "); Serial.println(brakeMotorVal);   */
   }
   else
   {
@@ -219,9 +224,9 @@ void motor_setValues (int throttle, int steering, int brake)
 
 void stopRobot ()
 {
-   throttleMotor.write(MOTOR_VALUE_THROTTLE_ZERO);
-   steeringMotor.write(MOTOR_VALUE_CENTER);
-   brakeMotor.write(MOTOR_VALUE_FULL_BRAKE);
+  throttleMotor.write(MOTOR_VALUE_THROTTLE_ZERO);
+  steeringMotor.write(MOTOR_VALUE_CENTER);
+  brakeMotor.write(MOTOR_VALUE_FULL_BRAKE);
 }
-  
-  
+
+
